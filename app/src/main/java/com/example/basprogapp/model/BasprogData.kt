a9a606ff8577fc36cadb46d3bfedbd0b36f66949
pierreviences <imaniyazid@gmail.com>
2023-10-31 10:35:43 +0700

    add: Add model basprog

--- app/src/main/java/com/example/basprogapp/model/BasprogData.kt
+++ app/src/main/java/com/example/basprogapp/model/BasprogData.kt
@@ -1,4 +1,15 @@
 package com.example.basprogapp.model
 
-class BasprogData {
+object BasprogData {
+    val basprogs = listOf(
+        Basprog(
+            "1",
+            "Python",
+            " Python adalah bahasa pemrograman tingkat tinggi yang populer, dikenal karena sintaksis yang mudah dibaca dan digunakan dalam berbagai aplikasi mulai dari pengembangan web hingga analisis data.",
+            "70,2",
+            "Guido Rossum",
+            "1991",
+            "ada"
+        ),
+    )
 }
