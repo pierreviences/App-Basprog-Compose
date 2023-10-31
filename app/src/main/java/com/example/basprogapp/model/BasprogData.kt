a5b978ecbf9900204e888d2929806036b779611c
pierreviences <imaniyazid@gmail.com>
2023-10-31 14:19:22 +0700

    feat: add data soruce

--- app/src/main/java/com/example/basprogapp/model/BasprogData.kt
+++ app/src/main/java/com/example/basprogapp/model/BasprogData.kt
@@ -9,7 +9,85 @@ object BasprogData {
             "70,2",
             "Guido Rossum",
             "1991",
-            "ada"
+            "https://i.ibb.co/fH0D0t3/top1.png"
         ),
+        Basprog(
+             "2",
+             "Javascript",
+             "JavaScript is a programming language commonly used for web development. It enables developers to create dynamic interactions on websites, changing page content in real-time, and providing interactive experiences to users.",
+             "66,4",
+             "Brendan Eich",
+            "1995",
+             "https://i.ibb.co/47X3Zxm/top2.png"
+        ),
+        Basprog(
+            "3",
+            "C++",
+            "C++ adalah bahasa pemrograman yang kuat dan serbaguna, merupakan pengembangan dari bahasa C. C++ mendukung pemrograman berorientasi objek, yang memungkinkan pengembang untuk mengorganisasi kode ke dalam objek yang dapat saling berinteraksi. Ini sering digunakan dalam pengembangan perangkat lunak yang memerlukan performa tinggi, seperti sistem operasi, permainan, dan aplikasi desktop.",
+            "64",
+            "Bjarne Stroustrup",
+            "1985",
+            "https://i.ibb.co/F8B8Srk/top3.png"
+        ),
+        Basprog(
+            "4",
+            "Go",
+            "Go adalah bahasa pemrograman yang dirancang oleh Google, fokus pada kesederhanaan, efisiensi, dan kecepatan kompilasi. Dengan sintaksis yang sederhana dan dukungan untuk konkurensi dan pemrograman paralel, Go cocok untuk pengembangan aplikasi yang harus menangani banyak tugas secara bersamaan, seperti layanan jaringan, pemrosesan data, dan mikrokontroler.",
+            "62,3",
+            "Rob Pike",
+            "2009",
+            "https://i.ibb.co/QJf2mJG/top4.png"
+        ),
+        Basprog(
+            "5",
+            "Ruby",
+            "Ruby adalah bahasa pemrograman yang terkenal dengan sintaksis yang elegan dan ekspresif. Dirancang untuk meningkatkan produktivitas pengembang, Ruby fokus pada kesederhanaan dan kenyamanan dalam menulis kode. Terkenal dengan kerangka kerjanya yang kuat, Ruby on Rails, bahasa ini sering digunakan untuk pengembangan web dan aplikasi berbasis server.",
+            "58,4",
+            "Yukihiro Matz",
+            "1995",
+            "https://i.ibb.co/2j7Kvhd/top5.png"
+        ),
+        Basprog(
+            "6",
+            "Swift",
+            "Swift adalah bahasa pemrograman yang dikembangkan oleh Apple untuk membuat aplikasi di platform iOS, macOS, watchOS, dan tvOS. Dikenal karena kinerja tinggi dan sintaksis yang mudah dibaca, Swift dirancang untuk meningkatkan efisiensi pengembangan dengan fitur-fitur modern seperti keselamatan tipe yang ketat, pemrograman berorientasi objek, dan dukungan untuk pemrograman fungsional.",
+            "56,6",
+            "Chris Lattner",
+            "2014",
+            "https://i.ibb.co/XZ6NXmT/top6.png6"
+        ),
+        Basprog(
+            "7",
+            "Java",
+            "Java adalah bahasa pemrograman yang kuat dan serbaguna, awalnya dikembangkan oleh Sun Microsystems (sekarang dimiliki oleh Oracle). Dikenal karena portabilitasnya yang tinggi, Java dapat dijalankan di berbagai platform tanpa perlu mengubah kode sumber. Java sering digunakan dalam pengembangan aplikasi berbasis server, aplikasi mobile (Android), perangkat lunak berbasis desktop, dan bahkan dalam pengembangan perangkat keras tertentu.",
+            "54,9",
+            "James Gosling",
+            "1995",
+            "https://i.ibb.co/crLSH9t/top7.png"
+        ),Basprog(
+            "8",
+            "Kotlin",
+            "Kotlin adalah bahasa pemrograman modern yang dapat berjalan di atas platform Java Virtual Machine (JVM). Dikembangkan oleh JetBrains, Kotlin dirancang untuk meningkatkan produktivitas pengembang dengan sintaksis yang lebih ekspresif, aman, dan mudah dibaca. Kotlin dapat digunakan untuk pengembangan aplikasi Android, server-side, web, dan berbagai jenis aplikasi lainnya yang mendukung JVM.",
+            "53,1",
+            "Andrey Breslav",
+            "2011",
+            "https://i.ibb.co/j3sMbxd/top8.png"
+        ),Basprog(
+            "9",
+            "PHP",
+            "PHP adalah bahasa pemrograman yang dirancang khusus untuk pengembangan web. PHP digunakan untuk membuat halaman web dinamis dengan kemampuan untuk berinteraksi dengan basis data, menghasilkan konten berdasarkan input pengguna, dan membuat aplikasi web yang kompleks. PHP sering kali dijalankan di sisi server dan bekerja sama dengan server web seperti Apache atau Nginx untuk menghasilkan halaman web yang dihasilkan secara dinamis.",
+            "48,7",
+            "Rasmus Lerdorf",
+            "1995",
+            "https://i.ibb.co/FJzJVWG/top9.png"
+        ), Basprog(
+            "10",
+            "C#",
+            "C# adalah sebuah bahasa pemrograman yang dikembangkan oleh Microsoft. Bahasa ini dirancang untuk memudahkan pengembangan aplikasi yang berjalan pada platform Windows, seperti aplikasi desktop, aplikasi web, dan aplikasi berbasis layanan. C# merupakan bahasa yang kuat dan modern, menggabungkan elemen-elemen dari C++ dan Java.",
+            "45,6",
+            "Anders Hejlsberg",
+            "2000",
+            "https://i.ibb.co/SyB0LLw/top10.png"
+        )
     )
 }
